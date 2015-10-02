75eef1d03ef2402797396dca0415340a06aed1ae
2090886l <ivailolafchiev@yahoo.com>
2015-10-03 00:59:54 +0100

    initial spawner

---
+++ MissileDodge/Assets/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+
+	public GameObject rocket;
+	// Use this for initialization
+	void Start () {
+
+		Vector3 spawnPosition = new Vector3 (0, 0, 0);
+		rocket =  (GameObject) Instantiate(rocket, spawnPosition, new Quaternion(0,0,0,0));
+		rocket.GetComponent<Rigidbody2D> ().velocity = new Vector2 (10, 0);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
