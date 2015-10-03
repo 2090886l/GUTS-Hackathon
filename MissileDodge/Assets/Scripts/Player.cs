f5aae7ab3f69cc7bea67d3846babab130576a77c
Ivan <2087678V@student.gla.ac.uk>
2015-10-03 21:55:56 +0100

    char animated

--- MissileDodge/Assets/Scripts/Player.cs
+++ MissileDodge/Assets/Scripts/Player.cs
@@ -15,7 +15,10 @@ public class Player : MonoBehaviour
 	private bool grounded = false;			// Whether or not the player is grounded.
 	private bool inCannon = false;
 	private Collider2D activeCannon;
-	
+	private Animator anim;
+	bool facingRight = true;
+	Rigidbody2D player;
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Cannon") {
 			inCannon = true;
@@ -32,11 +35,14 @@ public class Player : MonoBehaviour
 	{
 		// Setting up references.
 		groundCheck = transform.Find("groundCheck");
+		player = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	
 	void Update()
 	{
+
 
 
 		if (Input.GetMouseButtonDown(0)) //when left button is clicked
@@ -53,8 +59,13 @@ public class Player : MonoBehaviour
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
 		
 		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
+		if (Input.GetButtonDown ("Jump") && grounded) {
 			jump = true;
+			anim.SetBool("isGrounded", false);
+
+		}
+			
+
 	}
 	
 	
@@ -62,20 +73,32 @@ public class Player : MonoBehaviour
 	{
 		// Cache the horizontal input.
 		float h = Input.GetAxis("Horizontal");
+		float j = Input.GetAxisRaw ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs (h));
+
+		if (h > 0 && !facingRight)
+			Flip ();
+		if (h < 0 && facingRight)
+			Flip ();
 
 		
 		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
 			// ... add a force to the player.
+
 			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
 		
 		// If the player's horizontal velocity is greater than the maxSpeed...
 		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
 			// ... set the player's velocity to the maxSpeed in the x axis.
+
 			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
 		
 		// If the player should jump...
+
+
 		if(jump)
 		{
 
@@ -85,10 +108,19 @@ public class Player : MonoBehaviour
 			
 			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
 			jump = false;
+			anim.SetBool("isGrounded", true);
 		}
+
+
 	}
 	
-
+	void Flip() {
+		facingRight = !facingRight;
+		
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
 	
 
 }
